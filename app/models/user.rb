d95d8b59d1c16f01824ee05f6540929f3d74dda8
vivianne <viviannjoroge91@gmail.com>
2023-03-28 14:43:27 +0300

    add validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :hotels
+
+    validates :username,{
+        length:{maximum:6, minimum:9},
+        uniqueness:true,
+        presence:true
+    }
+
+    validates :email,{
+        uniqueness:true,
+        presence:true
+    }
+
 end
