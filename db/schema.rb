d14d95c336105c2146cd3c9f4c72b13c33d8b3c6
seansama <shaun.mwangi@student.moringaschool.com>
2023-03-28 14:31:03 +0300

    Run Hotel migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_091131) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_092015) do
+  create_table "hotels", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "rating", null: false
+    t.string "class", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.text "image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
