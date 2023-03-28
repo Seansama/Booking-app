c991e2072906fc2f65b57627063195f14fee87ba
seansama <shaun.mwangi@student.moringaschool.com>
2023-03-28 15:57:43 +0300

    Added foreign key to Hotels table from user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_114651) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_125303) do
   create_table "hotels", force: :cascade do |t|
     t.string "name", null: false
     t.integer "rating", null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_114651) do
     t.text "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_hotels_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_114651) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "hotels", "users"
 end
