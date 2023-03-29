01c40b79f2af8bea58794c9f6fad03c8a5c0a3ec
seansama <shaun.mwangi@student.moringaschool.com>
2023-03-29 11:19:57 +0300

    Review controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :reviews, only: [:index, :create, :destroy]
   resources :hotels
   post '/users', to: 'users#register'
   post '/users/login', to: 'users#login'
