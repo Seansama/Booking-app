3066faf51c5ee9b0a952c64c3bbcd051b5a44ae4
seansama <shaun.mwangi@student.moringaschool.com>
2023-03-30 15:26:41 +0300

    GET routes for booking and references migration file

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+  #before_action :authorize
+
+  def index
+    bookings = user.Booking.all
+    if bookings
+      render json: bookings, status: :ok
+    else
+      render json: {error: 'Bookings not available.'}, status: :not_found
+    end
+  end
+
+  def show
+    booking = user.Booking.find_by(id: params[:id])
+    if booking
+      render json: booking, status: :found
+    else
+      render json: {error: 'Booking does not seem to exist'}, status: :not_found
+    end
+  end
 end
