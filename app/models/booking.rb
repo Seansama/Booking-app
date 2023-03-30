7bd5fc7099e68df81c485a180d35dfa02a51574f
seansama <shaun.mwangi@student.moringaschool.com>
2023-03-30 11:25:25 +0300

    Updated model associations to include Booking Model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :hotel
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone_number, presence: true, length: { minimum: 10, maximum: 10 }
