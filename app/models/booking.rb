2a50ba73ef9e7c78e28be84ff07529b59c933e19
seansama <shaun.mwangi@student.moringaschool.com>
2023-03-30 11:16:14 +0300

    Completed Booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,7 @@
 class Booking < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true, length: { minimum: 10, maximum: 10 }
+  validates :check_in, presence: true , uniqueness: :true
+  validates :check_out, presence: :true, uniqueness: :true
 end
