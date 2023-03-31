fb6dd118a7d3488862084c9657c26a9b6790b1ef
vivianne <viviannjoroge91@gmail.com>
2023-03-31 15:22:04 +0300

    add the sign up route

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   resources :reviews, only: [:index, :create, :destroy]
   resources :hotels
-  post '/users', to: 'users#register'
-  post '/users/login', to: 'users#login'
-  post '/users/login/check', to: 'users#check_login_status'
 
-  # verify auth
+  # Sign-up and login routes
+  post '/signup', to: 'users#register'
+  post '/login', to: 'users#login'
+  post '/check_login_status', to: 'users#check_login_status'
+
+  # Verify auth route
   get '/authorize', to: 'application#authorize'
 
- 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
