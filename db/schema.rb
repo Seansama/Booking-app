7028fbe08a092219a77e5d39d23d5dbb324e1d74
seansama <shaun.mwangi@student.moringaschool.com>
2023-04-03 10:22:35 +0300

    Modifying hotel resources to correct fetch errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_001110) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_044341) do
   create_table "bookings", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_001110) do
   create_table "hotels", force: :cascade do |t|
     t.string "name", null: false
     t.integer "rating", null: false
-    t.string "class", null: false
+    t.string "hotel_class", null: false
     t.text "description", null: false
     t.integer "price", null: false
     t.text "image", null: false
