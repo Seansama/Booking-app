a893edf7ef3231b47a46c83d5d61b214805dd673
seansama <shaun.mwangi@student.moringaschool.com>
2023-04-04 10:29:55 +0300

    Config files for deployment

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
+  has_many :hotels
   has_many :bookings
   has_many :hotels, through: :bookings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_114243) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_072225) do
   create_table "bookings", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
